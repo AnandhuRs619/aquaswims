4a2b18b669708726a121439301a1937a3b1ba667
Manu-Madhu <manum66466@gmail.com>
2024-09-14 16:52:04 +0530

    Products

--- src/components/home/Products/ProductDetails.jsx
+++ src/components/home/Products/ProductDetails.jsx
@@ -1,18 +1,29 @@
 const ProductDetails = ({ product }) => {
   return (
-    <div className="mt-10 bg-gray-100 p-6 rounded-lg shadow-lg">
-      <h1 className="text-2xl font-semibold mb-4">Key Features</h1>
-      <h2 className="text-xl font-bold">{product.name}</h2>
-      <div className="grid grid-cols-2 gap-6 mt-4">
+    <div className="lg:mb-10 p-8 pb-20">
+      <h1 className="text-2xl font-semibold mb-4 lg:mb-10 uppercase">Key Features</h1>
+      <h2 className="text-xl font-medium uppercase text-[#000000]/90">Product Name : {product.name}</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-4">
         {product.keyFeatures.map((feature, index) => (
           <div key={index}>
-            <h3 className="text-lg font-semibold">{feature}</h3>
-            <p className="text-gray-600">
+            <h3 className="text-2xl font- text-[#000000]/70">{feature}</h3>
+            <p className="text-gray-600 text-sm pt-3">
               {index === 0
-                ? "Minimalist Aesthetics. Designed for style and functionality, perfect for both professionals and enthusiasts."
+                ? (
+                  <ul className="flex flex-col gap-1 pl-5 list-disc">
+                    <li>{`Minimalist Aesthetics. Designed for style and functionality, perfect for both professionals and enthusiasts.`}</li>
+                    <li>{`Compact and Lightweight: Easily portable for on-the-go photography`}</li>
+                  </ul>
+                )
                 : index === 1
-                ? "Compact and Lightweight. Easily portable for on-the-go photography."
-                : ""}
+                  ? (<ul className="flex flex-col gap-1 pl-5 list-disc">
+                    <li>{`Minimalist Aesthetics. Designed for style and functionality, perfect for both professionals and enthusiasts.`}</li>
+                    <li>{`Compact and Lightweight: Easily portable for on-the-go photography`}</li>
+                  </ul>)
+                  : (<ul className="flex flex-col gap-1 pl-5 list-disc">
+                    <li>{`Minimalist Aesthetics. Designed for style and functionality, perfect for both professionals and enthusiasts.`}</li>
+                    <li>{`Compact and Lightweight: Easily portable for on-the-go photography`}</li>
+                  </ul>)}
             </p>
           </div>
         ))}
