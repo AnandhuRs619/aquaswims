bb3cd63404037ada8b0140752d0937f1487aed48
Manu-Madhu <manum66466@gmail.com>
2024-09-14 16:56:48 +0530

    padding updated

--- src/components/home/Products/ProductDetails.jsx
+++ src/components/home/Products/ProductDetails.jsx
@@ -1,6 +1,6 @@
 const ProductDetails = ({ product }) => {
   return (
-    <div className="lg:mb-10 p-8 pb-20">
+    <div className="lg:mb-10 p-4 lg:p-8 pb-20">
       <h1 className="text-2xl font-semibold mb-4 lg:mb-10 uppercase">Key Features</h1>
       <h2 className="text-xl font-medium uppercase text-[#000000]/90">Product Name : {product.name}</h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-4">
